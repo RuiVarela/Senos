d461581069246c33c2a1cebd435780f086cdb301
Rui Varela <ryven.kicks.ass@gmail.com>
2023-02-22 20:15:46 +0000

    setup cpp compiler

--- engine/audio/Analyser.cpp
+++ engine/audio/Analyser.cpp
@@ -9,14 +9,26 @@ namespace sns {
 
 	void Analyser::configureGraph(int points, int duration) {
 		m_graph_points = points;
-		m_graph_duration = duration;
-		m_graph_duration_samples = int(samplesFromMicroseconds(duration));
+
+		m_graph_duration = std::min(duration, SamplesDuration);
+		m_graph_duration_samples = int(samplesFromMilliseconds(m_graph_duration));
+
+
+		//std::vector<float> x;
+		//generateGraph(x);
 	}
 
 	void Analyser::generateGraph(std::vector<float>& points) {
+		if (int(points.size()) != m_graph_points)
+			points.resize(m_graph_points);
 
-
+		float increment = float(m_graph_duration_samples) / float(m_graph_points);
 		
+		for (int i = 0; i != m_graph_points; ++i) {
+			float p0 = 0.0f;
+			float pf = modf(float(i) * increment, &p0);
+			float p1 = p0 + 1.0f;
+		}
 	}
 
 	bool Analyser::isAccepting() {
